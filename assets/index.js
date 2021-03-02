918d73b02126879b169cb4cfaa444f529e835a3a
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-03-02 10:41:50 -0300

    video auto pause when scroll on screen

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,12 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('video')
 const controllBTN = document.querySelector('#playButton')
 const muteBTN = document.querySelector('#muteButton')
 
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] })
 
 controllBTN.onclick = () => player.playToggle()
 video.onclick = () => player.playToggle()
