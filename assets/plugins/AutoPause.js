0b46634e14d9acf618ccb51bc7e1a91f7cfa5791
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-03-02 11:27:16 -0300

    add video auto pause whit visibilityChange event

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,25 +2,38 @@ class AutoPause {
 	constructor() {
 		this.threshold = 0.25
 		this.handlerIntersection = this.handlerIntersection.bind(this)
+		this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this)
 	}
 
 	run(player) {
 		this.player = player
 		const observer = new IntersectionObserver(this.handlerIntersection, { threshold: this.threshold })
 		observer.observe(this.player.media)
+
+		document.addEventListener('visibilitychange', this.handlerVisibilityChange)
 	}
 
 	handlerIntersection(entries) {
 		const entry = entries[0]
 		
 		const isVisible = entry.intersectionRatio >= this.threshold
-		
+
 		 if(isVisible) {
 		 	this.player.play()
 		 } else {
 		 	this.player.pause()
 		 }
 
+	}
+
+	handlerVisibilityChange() {
+		const isVisible = document.visisbilityState === 'visible'
+
+		if(isVisible) {
+			this.player.play()
+		} else {
+			this.player.pause()
+		}
 	}
 }
 
